2e02d78ad4e1d5425ad7940a26f3479ec0c31872
Simon McDonnell <simjmcd@gmail.com>
2017-01-23 22:53:36 +0000

    Experiments with vertex buffers, implemented message pump

--- Source/Application/main.cpp
+++ Source/Application/main.cpp
@@ -82,14 +82,59 @@ int main()
 
 	//shaderCache.AddShader()
 
-	const GLfloat verts[] = { 0.f, 0.5f, 0.f, 1.f, 1.f, 0.f,
-		-0.5f, -0.5f, 0.f, 1.f, 0.f, 0.f,
-		0.5f, -0.5f, 0.f, 1.f, 0.f, 1.f };
+	const GLfloat vertPositions[] = { 0.f, 0.5f, 0.f,
+		-0.5f, -0.5f, 0.f,
+		0.5f, -0.5f, 0.f };
+
+	const GLfloat vertColours[] = { 0.f, 1.f, 0.f, 1.0f,
+		0.f, 0.f, 1.f, 1.0f,
+		0.f, 1.f, 0.f, 1.0f };
+
+	const GLfloat vertsCombined[] = { -0.5f, 0.5f, 0.f, 0.f, 1.f, 0.f, 1.0f,
+		-1.f, -0.5f, 0.f, 0.f, 0.f, 1.f, 1.0f,
+		0.f, -0.5f, 0.f, 0.f, 1.f, 0.f, 1.0f };
 
 	GLuint vbo;
 	//glCreateBuffers(sizeof(GLfloat) * 9, &vbo);
 
-	glGenBuffers(1, &vbo);
+	GLuint buffers[2];
+	glCreateBuffers(2, &buffers[0]);
+
+	glNamedBufferStorage(buffers[0], sizeof(vertPositions), vertPositions, 0);
+	glVertexArrayVertexBuffer(vao, 0, buffers[0], 0, 3 * sizeof(GLfloat));
+	GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+	glVertexArrayAttribFormat(vao, posAttrib, 3, GL_FLOAT, GL_FALSE, 0);
+	glVertexArrayAttribBinding(vao, posAttrib, 0);
+	glEnableVertexAttribArray(posAttrib);
+
+
+	glNamedBufferStorage(buffers[1], sizeof(vertColours), vertColours, 0);
+	glVertexArrayVertexBuffer(vao, 1, buffers[1], 0, 4 * sizeof(GLfloat));
+	GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
+	glVertexArrayAttribFormat(vao, colAttrib, 4, GL_FLOAT, GL_FALSE,0);
+	glVertexArrayAttribBinding(vao, colAttrib, 1);
+	glEnableVertexAttribArray(colAttrib);
+
+
+	GLuint vao2;
+	glCreateVertexArrays(1, &vao2);
+	glBindVertexArray(vao2);
+
+	glCreateBuffers(1, &vbo);
+	glNamedBufferStorage(vbo, sizeof(vertsCombined), vertsCombined, 0);
+	posAttrib = glGetAttribLocation(shaderProgram, "position");
+	glVertexArrayAttribFormat(vao2, posAttrib, 3, GL_FLOAT, GL_FALSE, 0);
+	glVertexArrayAttribBinding(vao2, posAttrib, 0);
+	glEnableVertexAttribArray(posAttrib);
+
+	colAttrib = glGetAttribLocation(shaderProgram, "color");
+	glVertexArrayAttribFormat(vao2, colAttrib, 4, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat));
+	glVertexArrayAttribBinding(vao2, colAttrib, 0);
+	glEnableVertexAttribArray(colAttrib);
+
+	glVertexArrayVertexBuffer(vao2, 0, vbo, 0, 7 * sizeof(GLfloat));
+
+	/*glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
 	GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
@@ -100,11 +145,18 @@ int main()
 	glEnableVertexAttribArray(colAttrib);
 	glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
 
-	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 18, &(verts), GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 18, &(verts), GL_STATIC_DRAW);*/
 	//glBindVertexBuffer()
 
 	//A sleepy rendering loop, wait for 3 seconds and render and present the screen each time
 	while(true) {
+
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) {
+			/* handle your event here */
+		}
+
+
 		//First clear the renderer
 		//SDL_RenderClear(ren);
 		////Draw the texture
@@ -112,7 +164,8 @@ int main()
 		////Update the screen
 		//SDL_RenderPresent(ren);
 
-		const GLfloat red[] = { sin(SDL_GetTicks() * 0.001f) * 0.5f + 0.5f, cos(SDL_GetTicks() * 0.001f) * 0.5f + 0.5f, 0.0f, 1.0f };
+		//const GLfloat red[] = { sin(SDL_GetTicks() * 0.001f) * 0.5f + 0.5f, cos(SDL_GetTicks() * 0.001f) * 0.5f + 0.5f, 0.0f, 1.0f };
+		const GLfloat red[] = { 1.f, 0.0f, 0.0f, 1.0f };
 
 		
 
@@ -121,10 +174,15 @@ int main()
 
 		GLfloat offset[] = { sin(SDL_GetTicks() * 0.001f) * 0.5f, cos(SDL_GetTicks() * 0.001f) * 0.5f, 0.0f };
 
-		glVertexAttrib3fv(0, offset);
+		//glVertexAttrib3fv(0, offset);
 
 		glPointSize(5.f);
 		glPatchParameteri(GL_PATCH_VERTICES, 3);
+
+		glBindVertexArray(vao);
+		glDrawArrays(GL_PATCHES, 0, 3);
+
+		glBindVertexArray(vao2);
 		glDrawArrays(GL_PATCHES, 0, 3);
 
 		SDL_GL_SwapWindow(win);
