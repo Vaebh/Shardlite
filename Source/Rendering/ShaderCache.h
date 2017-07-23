4af8025c4d0f7ecccfd405451e8408ec1e32837e
Simon McDonnell <simjmcd@gmail.com>
2017-07-23 23:53:08 +0100

    Added comment to shader cache about future plans

--- Source/Rendering/ShaderCache.h
+++ Source/Rendering/ShaderCache.h
@@ -1,3 +1,16 @@
+// We should be able to query the shader for specific attributes and their setup info like binding indexes, sizes, and types
+// We'll be able to do this because when a shader is loaded we'll scan its contents and determine what attributes are specified there
+// We will match these by name against a list of predetermined attribute names that we support, and will fail the shader creation if there is an attribute mismatch
+// As long as there is one valid attribute then it can go ahead, but we wil log that there were unspported types or names in the shader
+
+// This hardcoded support will allow us to know exactly what they need for their setup.
+
+// We can do the same for uniforms.
+
+// We can use this info to then do vertex attribute setup in a much nicer way, which is dependant on the actual shaders and what
+// they specifically need (as opposed to needing all of them to support all things right now)
+
+
 #ifndef SHADER_CACHE
 #define SHADER_CACHE
 
