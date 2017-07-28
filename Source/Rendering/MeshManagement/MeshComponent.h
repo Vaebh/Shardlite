39c5cce523f4b37fafe7dcfb2e2ae4065f92fcd9
Simon McDonnell <simjmcd@gmail.com>
2017-07-28 23:05:49 +0100

    Added shader pointer and vertex attribute binding logic to MeshComponent

--- Source/Rendering/MeshManagement/MeshComponent.h
+++ Source/Rendering/MeshManagement/MeshComponent.h
@@ -2,6 +2,13 @@
 #define MESH_COMPONENT_SHARDLITE
 
 #include "../../EntityComponent/Component.h"
+
+// Extract Shader from ShaderCache
+#include "../ShaderCache.h"
+
+// Extract AttributeType from ShaderParser
+#include "../ShaderParser.h"
+
 #include <vector>
 
 class Mesh;
@@ -9,11 +16,11 @@ class Mesh;
 class MeshComponent : public Component
 {
 public:
-	MeshComponent(Mesh* mesh);
+	MeshComponent(Mesh* mesh, Shader* shader);
 
 	bool IsTransparent() {return m_isTransparent;}
 
-	inline int GetShaderId() { return m_shaderId; }
+	inline Shader* GetShader() { return m_shader; }
 	inline std::vector<int> GetTextureIds() { return m_textureIds; }
 
 	inline Mesh* GetMesh() { return m_mesh; }
@@ -21,16 +28,22 @@ public:
     inline bool IsStatic() {return m_static;}
     inline bool HasTransparency() {return m_hasTransparency;}
 
+	void CreateVertexBuffers();
+	void BindVertexData(AttributeType attributeType, VertexAttribute& vertexAttribute, const std::vector<GLfloat>& attributeData);
+
 private:
 	Mesh* m_mesh;
 
     bool m_static;
     bool m_hasTransparency;
     
-	int m_shaderId;
+	Shader* m_shader;
 	std::vector<int> m_textureIds;
 
 	bool m_isTransparent;
+
+	GLuint m_vao;
+	GLuint m_vertexAttributeVBOs[AllowedAttributeArraySize] {0};
 };
 
 #endif
