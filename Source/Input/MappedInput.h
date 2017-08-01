4af33368a6b60fe6d98b228c360a2f1313ab739e
Simon McDonnell <simjmcd@gmail.com>
2017-08-01 23:26:25 +0100

    Added first iteratio of mapped input file

---
+++ Source/Input/MappedInput.h
@@ -0,0 +1,27 @@
+#ifndef MAPPED_INPUT_SHARDLITE
+#define MAPPED_INPUT_SHARDLITE
+
+#include "RawInputConstants.h"
+#include "InputConstants.h"
+
+#include <glm.hpp>
+
+enum InputType
+{
+	KeyDown,
+	KeyUp,
+	KeyHeld,
+	Range
+};
+
+struct MappedInput
+{
+	RawInput m_rawInput;
+	Input m_processedInput;
+	InputType m_inputType;
+
+	int m_intInputValue;
+	glm::vec2 m_rangeInputValue;
+};
+
+#endif
