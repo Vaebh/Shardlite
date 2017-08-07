ef0ddf748893f1287e5691bf43d9f35bc97d9ef4
Simon McDonnell <simjmcd@gmail.com>
2017-08-07 18:01:38 +0100

    Initializing timer, effect active, and controller pointer array fields

--- Source/Systems/HapticsSystem.cpp
+++ Source/Systems/HapticsSystem.cpp
@@ -8,6 +8,9 @@
 
 int HapticsSystem::StartUp()
 {
+	m_effectTimer = 0.f;
+	m_effectActive = false;
+
 	return SETUP_SUCCESS;
 }
 
@@ -28,41 +31,82 @@ void HapticsSystem::SetHapticHandles(SDL_GameController* in_controllerHandles[MA
 {
 	for (int i = 0; i < numControllers; ++i)
 	{
+		m_controllerHandles[i] = in_controllerHandles[i];
+
 		SDL_Joystick* joystickHandle = SDL_GameControllerGetJoystick(in_controllerHandles[i]);
 		m_hapticHandles[i] = SDL_HapticOpenFromJoystick(joystickHandle);
 	}
 }
 
+void HapticsSystem::Update(float deltaTime)
+{
+	if (!m_effectActive)
+	{
+		return;
+	}
+
+	m_effectTimer += deltaTime;
+
+	if (m_effectTimer >= m_currentEffect.m_duration)
+	{
+		m_effectActive = false;
+	}
+}
+
 void HapticsSystem::PlayHapticEffect(HapticEffectType effectType, int controllerIndex, float duration, float strength, float secondaryStrength, bool overrideCurrentEffect)
 {
-	if (m_effectActive && overrideCurrentEffect)
+	if (m_effectActive)
 	{
-		EndCurrentEffect(effectType, controllerIndex);
+		if (overrideCurrentEffect && (effectType != m_currentEffect.m_type && m_currentEffect.m_type != Invalid))
+		{
+			EndCurrentEffect(m_currentEffect.m_type, controllerIndex);
+		}
+		else
+		{
+			return;
+		}
 	}
+	else if (effectType != m_currentEffect.m_type && m_currentEffect.m_type != Invalid)
+	{
+		EndCurrentEffect(m_currentEffect.m_type, controllerIndex);
+	}
+
+	bool needToInit = effectType != m_currentEffect.m_type;
 
 	m_currentEffect.m_type = effectType;
 	m_currentEffect.m_duration = duration;
 	m_currentEffect.m_strength = strength;
 	m_currentEffect.m_secondaryStrength = secondaryStrength;
+	m_currentEffect.m_controllerIndex = controllerIndex;
 
 	switch (effectType)
 	{
 		case SimpleRumble:
-			InitSimpleRumbleEffect(controllerIndex);
+			if (needToInit)
+			{
+				InitSimpleRumbleEffect(controllerIndex);
+			}
 			PlaySimpleRumbleEffect(controllerIndex);
 			break;
 
 		case LeftRight:
-			InitLeftRightEffect(controllerIndex);
+			if (needToInit)
+			{
+				InitLeftRightEffect(controllerIndex);
+			}
 			PlayLeftRightEffect(controllerIndex);
 			break;
 	}
+
+	m_effectActive = true;
+	m_effectTimer = 0.f;
 }
 
 void HapticsSystem::InitSimpleRumbleEffect(int controllerIndex)
 {
-	m_currentEffect.m_effectId = -1;
-	SDL_HapticRumbleInit(m_hapticHandles[controllerIndex]);
+	std::cout << SDL_HapticRumbleInit(m_hapticHandles[controllerIndex]) << std::endl;
+
+	printf("SDL_HapticNewEffect failed: %s\n", SDL_GetError());
 }
 
 void HapticsSystem::InitLeftRightEffect(int controllerIndex)
